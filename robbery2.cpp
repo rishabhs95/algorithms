e6a25d9fc328da9fa19c20e42aa434a4ed1bf990
Rishabh Saxena <orishabh1@gmail.com>
2017-05-28 20:09:50 +0530

    Add robbery2.cpp

---
+++ robbery2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+int main(void)
+{
+	int test;
+	scanf("%d",&test);
+	for (int i=0; i<test; i++)
+	{
+		int bandits;
+		unsigned long long int coins, hissa[bandits];
+		scanf("%llu %d", &coins, &bandits);
+		for (int j=0; j<bandits; j++)
+		{
+			hissa[j] = 
+		}
+	}
+	return 0;
+}
