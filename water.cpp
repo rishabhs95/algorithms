61b2bfdbaa54a8081500b281cad411ec83979536
Rishabh Saxena <orishabh1@gmail.com>
2017-06-07 20:09:51 +0530

    Add water.cpp

---
+++ water.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <bitset>
+#include <string>
+#include <vector>
+#include <utility>
+#include <list>
+#include <set>
+#include <map>
+
+using namespace std;
+
+int main() {
+	freopen("../input.txt", "r", stdin);
+	// freopen("../output.txt", "w", stdout);
+	
+	return 0;
+}
