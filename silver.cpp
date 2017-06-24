e7e69b3fc3924975ec0288a4043d8bfa116b6d11
Rishabh Saxena <orishabh1@gmail.com>
2017-06-24 20:09:54 +0530

    Add silver.cpp

---
+++ silver.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <bitset>
+#include <string>
+#include <vector>
+#include <utility>
+#include <cstdio>
+#include <queue>
+#include <cmath>
+#include <list>
+#include <set>
+#include <map>
+
+using namespace std;
+
+int main() {
+	freopen("input.txt", "r", stdin);
+	int n;
+	while (cin >> n && n!= 0) {
+		
+	}
+	return 0;
+}
