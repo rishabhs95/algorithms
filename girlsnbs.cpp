50f571004926c74ca33d8a677c4ffdfedcf36df2
Rishabh Saxena <orishabh1@gmail.com>
2017-07-25 20:09:58 +0530

    Add girlsnbs.cpp

---
+++ girlsnbs.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstring> //memset
+#include <algorithm>
+#include <bitset>
+#include <string>
+#include <vector>
+#include <utility>
+#include <cstdio>
+#include <queue>
+#include <cmath>
+#include <list>
+#include <set>
+#include <map>
+
+using namespace std;
+
+int main() {
+	freopen("input.txt", "r", stdin);
+	int a, b;
+	while (cin >> a >> b && a != -1 && b != -1)	{
+		
+	}
+	return 0;
+}
