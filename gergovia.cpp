fc1177414b2d2baeb15c6c41e8bf47986342d0a6
Rishabh Saxena <orishabh1@gmail.com>
2017-07-29 20:09:58 +0530

    Add gergovia.cpp

---
+++ gergovia.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstring> //memset
+#include <algorithm>
+#include <bitset>
+#include <string>
+#include <vector>
+#include <utility>
+#include <cstdio>
+#include <queue>
+#include <cmath>
+#include <list>
+#include <set>
+#include <map>
+
+using namespace std;
+
+int main() {
+	freopen("input.txt", "r", stdin);
+	int houses, i, bottles, cumulative, sum;
+	while (cin >> houses && houses != 0) {
+		cumulative = 0; sum = 0;
+		for (i = 0; i < houses; i++) {
+			cin >> bottles;
+			
+		}
+		cout << sum << endl;
+	}
+	return 0;
+}
