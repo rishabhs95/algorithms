7cb89abc7b7a27baedefa87f109d936cd0b26a73
Rishabh Saxena <orishabh1@gmail.com>
2017-08-01 20:09:59 +0530

    Add speed.cpp

---
+++ speed.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstring> //memset
+#include <algorithm>
+#include <bitset>
+#include <string>
+#include <vector>
+#include <utility>
+#include <cstdio>
+#include <queue>
+#include <cmath>
+#include <list>
+#include <set>
+#include <map>
+
+using namespace std;
+
+int gcd (int a, int b) {
+	if (b==0) return a;
+	return gcd(b, a%b);
+}
+
+int main() {
+	freopen("input.txt", "r", stdin);
+	int test_cases, a, b, hcf;
+	cin >> test_cases;
+	while (test_cases--) {
+		cin >> a >> b;
+		if (a*b > 0) {
+			hcf = gcd(max(a, b), min(a, b));
+			cout << (a*b)/hcf << endl;
+		}
+		else {
+			
+		}
+	}
+	return 0;
+}
