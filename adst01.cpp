79b453d64d4d25ef1929fe3dcc3e1757f8b12363
Rishabh Saxena <orishabh1@gmail.com>
2017-08-19 20:10:01 +0530

    Add adst01.cpp

---
+++ adst01.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+typedef long long int ll;
+const int mod = 1e9+7;
+
+int truncky_sum (ll n) {
+	int res;
+	
+	return res;
+}
+
+int main() {
+	freopen("input.txt", "r", stdin);
+	int test_cases;
+	ll n;
+	cin >> test_cases;
+	while (test_cases--) {
+		cin >> n;
+		cout << truncky_sum(n) << endl;
+	}
+	return 0;
+}
