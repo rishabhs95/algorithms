4810cd43e414298f1e6b5b93a0512178bfac8c01
Rishabh Saxena <orishabh1@gmail.com>
2017-08-30 20:10:02 +0530

    Add amr11f.cpp

---
+++ amr11f.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstring> //memset
+#include <algorithm>
+#include <bitset>
+#include <string>
+#include <vector>
+#include <utility>
+#include <cstdio>
+#include <queue>
+#include <cmath>
+#include <list>
+#include <set>
+#include <map>
+
+using namespace std;
+
+int main() {
+	freopen("input.txt", "r", stdin);
+	int test_cases, n, m, f, sb, sf, db, df, time, Q;
+	cin >> test_cases;
+	while (test_cases--) {
+		cin >> n >> f >> m;
+		
+		while (m--) {
+			cin >> sb >> sf >> db >> df >> time;
+
+		}
+		cin >> Q;
+		while (Q--) {
+			cin >> sb >> sf >> db >> df;
+			// print minimum path
+		}
+	}
+	return 0;
+}
