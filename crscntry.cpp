f3fc4fc92524b1838ac4eee327e3acca6e66cc2d
Rishabh Saxena <orishabh1@gmail.com>
2017-09-05 20:10:03 +0530

    Add crscntry.cpp

---
+++ crscntry.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstring> //memset
+#include <algorithm>
+#include <bitset>
+#include <string>
+#include <vector>
+#include <utility>
+#include <cstdio>
+#include <queue>
+#include <cmath>
+#include <list>
+#include <set>
+#include <map>
+
+using namespace std;
+
+int main() {
+	freopen("input.txt", "r", stdin);
+	int test_cases, temp;
+	cin >> test_cases;
+	while (test_cases--) {
+		while (cin >> temp && temp != 0) {
+			
+			while (cin >> temp && temp != 0) {
+				
+			}
+		}
+	}
+	return 0;
+}
