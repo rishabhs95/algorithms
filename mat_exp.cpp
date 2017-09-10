fb559d753d8098a79e5b894509440e61d583beb4
Rishabh Saxena <orishabh1@gmail.com>
2017-09-10 20:10:04 +0530

    Add mat_exp.cpp

---
+++ mat_exp.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstring> //memset
+#include <algorithm>
+#include <bitset>
+#include <string>
+#include <vector>
+#include <utility>
+#include <cstdio>
+#include <queue>
+#include <cmath>
+#include <list>
+#include <set>
+#include <map>
+
+using namespace std;
+
+int main() {
+	freopen("input.txt", "r", stdin);
+	int test_cases;
+	cin >> test_cases;
+	while (test_cases--) {
+		
+	}
+	return 0;
+}
